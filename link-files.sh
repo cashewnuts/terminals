f539878280d5118fba3e38425bd0d7d6372a7b8e
parallels <parallels@fedora-30.shared>
2019-11-26 12:36:54 +0900

    FIX .tmux.remote.conf is not linked by script.

--- link-files.sh
+++ link-files.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
 ln -s -f $PWD/.{aliases,profile,zshrc} ~/
-ln -s -f $PWD/tmux/.tmux.conf ~/
+ln -s -f $PWD/tmux/.{tmux,tmux.remote}.conf ~/
 
 ln -s -f $PWD/{nvim,vifm} ~/.config/
