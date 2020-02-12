8f2c0213552c571bf8b874a69fe995cebfe48b11
Tatsuya Kanemoto <cashewnuts903@gmail.com>
2020-02-12 23:45:45 +0900

    UPDATE powerlevel config.

--- link-files.sh
+++ link-files.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-ln -s -f $PWD/.{aliases,zprofile,zshrc} ~/
+ln -s -f $PWD/.{aliases,zprofile,zshrc,p10k.zsh} ~/
 ln -s -f $PWD/tmux/.{tmux,tmux.remote}.conf ~/
 
 ln -s -f $PWD/{nvim,vifm,alacritty} ~/.config/
