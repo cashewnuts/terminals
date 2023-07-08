b6d55e25e892ee481077475655bf3a92332f7cde
Tatsuya Kanemoto <cashewnuts903@gmail.com>
2023-07-08 15:58:52 +0900

    remove alacritty config.

--- link-files.sh
+++ link-files.sh
@@ -3,4 +3,4 @@
 ln -s -f $PWD/.{aliases,zprofile,zshrc,p10k.zsh} ~/
 ln -s -f $PWD/tmux/.{tmux,tmux.remote}.conf ~/
 
-ln -s -f $PWD/{nvim,vifm,alacritty} ~/.config/
+ln -s -f $PWD/{nvim,vifm} ~/.config/
